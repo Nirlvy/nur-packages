e95a86f6cc9514cbd834444d3812b47cbf3f1ec6
Nirlvy <nirlvy@qq.com>
2024-07-14 01:09:38 +0800

    fix: aris-plymouth.script

--- pkgs/aris-plymouth/default.nix
+++ pkgs/aris-plymouth/default.nix
@@ -1,13 +1,15 @@
 { stdenv, lib }:
 stdenv.mkDerivation {
   pname = "aris-plymouth";
-  version = "2024.7.7";
+  version = "2024.7.14";
 
   src = ./aris;
 
   installPhase = ''
     mkdir -p $out/share/plymouth/themes
     cp -r $src $out/share/plymouth/themes/aris
+    chmod -R 755 $out
+    find $out/share/plymouth/themes/aris -name aris.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
   '';
 
   meta = with lib; {
